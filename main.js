31bd8e25036bb98250851ebe23dc1ae26bac9ee2
unknown <iliailia@me.com>
2022-01-26 03:32:02 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+
+const api_key = 'AIzaSyAvexm-2pkBk6Ub37J3_2yKDAdjWCvxpF4';
+const business_type = 'restaurant';
+const location = '50.110924, 8.682127';
+
+let get_places_id_config = {
+  method: 'get',
+  url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${business_type}&location=${location}&radius=10000&key=${api_key}`,
+  headers: {},
+};
+
+const id_filter = (e) => {
+  return e.place_id;
+};
+
+const getEmails = async () => {
+  const get_places = await axios(get_places_id_config)
+    .then(function (response) {
+      const data = response.data.results;
+      return data.map(id_filter);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  const get_websites = await get_places.map(async (e) => {
+    const data = await axios({
+      method: 'get',
+      url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${e}&key=${api_key}`,
+      headers: {},
+    });
+
+    return data.data.result.website;
+  });
+
+  const data = await Promise.all(get_websites);
+
+  console.log(data);
+};
+
+getEmails();
